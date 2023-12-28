51e54d14d15343a2db587176185bea1c85550abc
Aryaardhra <ardhra455@gmail.com>
2023-12-28 16:12:10 +0530

    hotel path

--- src/components/pages/hotel/Hotel.js
+++ src/components/pages/hotel/Hotel.js
@@ -21,7 +21,7 @@ const Hotel = () => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
   const [openModel, setOpenModel] = useState(false);
-  const { data, loading, error } = useFetch(`/find/${id}`);
+  const { data, loading, error } = useFetch(`hotels/find/${id}`);
   
   const { user } = useContext(AuthContext); 
   const navigate = useNavigate();
